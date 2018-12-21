4bee7a409d03d192c8a1d5c1dbfe9d08d2b93059
fawwazramzy <rahanb37@gmail.com>
2018-12-21 07:06:46 +0700

    update

--- src/Pilihan/Pilihan.java
+++ src/Pilihan/Pilihan.java
@@ -66,7 +66,7 @@ public class Pilihan extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("jButton1");
+        jButton1.setText("OK");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
